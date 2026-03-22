becd6ab3bdf7c17db6948eaa2a297ff591da7859
Aman Sono <130969819+Amansono@users.noreply.github.com>
2026-03-22 12:52:32 +0530

    Add Streamlit UI for Assam Panchayat Scraper

--- web-ui.py
+++ web-ui.py
@@ -1,1 +1,16 @@
+import streamlit as st
+from scraper_logic import AssamVoterScraper
 
+st.title("Assam Panchayat Electoral Roll Scraper")
+
+# User Inputs
+dist = st.sidebar.text_input("Enter District")
+zilla = st.sidebar.text_input("Enter Zilla Parishad")
+gp = st.sidebar.text_input("Enter GP Name")
+ward = st.sidebar.text_input("Enter Ward")
+
+if st.button("Fetch List"):
+    with st.spinner('Scraping data... please wait.'):
+        scraper = AssamVoterScraper()
+        result = scraper.get_data(dist, zilla, gp, ward)
+        st.success(result)
